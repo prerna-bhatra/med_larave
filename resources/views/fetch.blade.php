f33087ed9aa8e54dfe52eb5cb0116c37a230ff64
prerna <bhatraprerna061@gmail.com>
2020-03-30 19:27:38 +0530

    livesearch

---
+++ resources/views/fetch.blade.php
@@ -0,0 +1,7 @@
+@foreach($seachbyphone as  $data)  
+      <a href="#">{{$data->name}}</a> <br>            
+@endforeach
+<?php
+
+//echo $seachbyphone;
+?>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/fetch', function () {
+    return view('fetch');
+});
 
 Auth::routes();
 
